29a163ba0defad1933a441b4f8de6407723a4877
BaoTrung <baotrung372k@gmail.com>
2024-01-18 23:31:41 +0700

    add button color

--- invoice_app/lib/config/color_config.dart
+++ invoice_app/lib/config/color_config.dart
@@ -11,4 +11,22 @@ class DarkColors {
 
   static const backgroundPrimary = Color(0xFF141625);
   static const backgroundSecondary = Color(0xFF1E2139);
+
+  static const button1Color = Color(0xFF7C5DFA);
+  static const hoverButton1Color = Color(0xFF9277FF);
+
+  static const button2Color = Color(0xFF7C5DFA);
+  static const hoverButton2Color = Color(0xFF9277FF);
+
+  static const button3Color = Color(0xFF141625);
+  static const hoverButton3Color = Color(0xFFFFFFFF);
+
+  static const button4Color = Color(0xFF252945);
+  static const hoverButton4Color = Color(0xFF1E2139);
+
+  static const button5Color = Color(0xFFEC5757);
+  static const hoverButton5Color = Color(0xFF9277FF);
+
+  static const button6Color = Color(0xFFF8F8FB);
+  static const hoverButton6Color = Color(0xFFDFE3FA);
 }
--- invoice_app/lib/presentation/resources/app_colors.dart
+++ invoice_app/lib/presentation/resources/app_colors.dart
@@ -9,6 +9,18 @@ class AppColors {
   final Color action;
   final Color backgroundPrimary;
   final Color backgroundSecondary;
+  final Color button1Color;
+  final Color hoverButton1Color;
+  final Color button2Color;
+  final Color hoverButton2Color;
+  final Color button3Color;
+  final Color hoverButton3Color;
+  final Color button4Color;
+  final Color hoverButton4Color;
+  final Color button5Color;
+  final Color hoverButton5Color;
+  final Color button6Color;
+  final Color hoverButton6Color;
 
   AppColors({
     required this.textPrimary,
@@ -16,6 +28,18 @@ class AppColors {
     required this.action,
     required this.backgroundPrimary,
     required this.backgroundSecondary,
+    required this.button1Color,
+    required this.hoverButton1Color,
+    required this.button2Color,
+    required this.hoverButton2Color,
+    required this.button3Color,
+    required this.hoverButton3Color,
+    required this.button4Color,
+    required this.hoverButton4Color,
+    required this.button5Color,
+    required this.hoverButton5Color,
+    required this.button6Color,
+    required this.hoverButton6Color,
   });
 }
 
@@ -25,6 +49,18 @@ final colorsDark = AppColors(
   action: DarkColors.action,
   backgroundPrimary: DarkColors.backgroundPrimary,
   backgroundSecondary: DarkColors.backgroundSecondary,
+  button1Color: DarkColors.button1Color,
+  hoverButton1Color: DarkColors.hoverButton1Color,
+  button2Color: DarkColors.button2Color,
+  hoverButton2Color: DarkColors.hoverButton2Color,
+  button3Color: DarkColors.button3Color,
+  hoverButton3Color: DarkColors.hoverButton3Color,
+  button4Color: DarkColors.button4Color,
+  hoverButton4Color: DarkColors.hoverButton4Color,
+  button5Color: DarkColors.button5Color,
+  hoverButton5Color: DarkColors.hoverButton5Color,
+  button6Color: DarkColors.button6Color,
+  hoverButton6Color: DarkColors.hoverButton6Color,
 );
 
 extension AppColorsExtension on BuildContext {
