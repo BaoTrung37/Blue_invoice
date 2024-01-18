f1d4760f299f030f8397119e4000427efaecdeff
BaoTrung <baotrung372k@gmail.com>
2024-01-18 22:39:59 +0700

    add background color

--- invoice_app/lib/presentation/pages/home/home_screen.dart
+++ invoice_app/lib/presentation/pages/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:invoice_app/presentation/resources/app_colors.dart';
 
 @RoutePage()
 class HomeScreen extends StatelessWidget {
@@ -7,8 +8,9 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      backgroundColor: context.colors.backgroundPrimary,
+      body: const Center(
         child: Text('Home Screen'),
       ),
     );
