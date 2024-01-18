645dcc602c82227be0a88e2002dd30b3b4d70f43
BaoTrung <baotrung372k@gmail.com>
2024-01-18 22:36:54 +0700

    add common color

--- invoice_app/lib/config/color_config.dart
+++ invoice_app/lib/config/color_config.dart
@@ -6,4 +6,9 @@ class DarkColors {
   /// Text
   static const textPrimary = Color(0xFFFFFFFF);
   static const textSecondary = Color(0xFF85859F);
+
+  static const action = Color(0xFFDFE3FA);
+
+  static const backgroundPrimary = Color(0xFF141625);
+  static const backgroundSecondary = Color(0xFF1E2139);
 }
--- invoice_app/lib/presentation/resources/app_colors.dart
+++ invoice_app/lib/presentation/resources/app_colors.dart
@@ -6,16 +6,25 @@ import 'package:invoice_app/config/color_config.dart';
 class AppColors {
   final Color textPrimary;
   final Color textSecondary;
+  final Color action;
+  final Color backgroundPrimary;
+  final Color backgroundSecondary;
 
   AppColors({
     required this.textPrimary,
     required this.textSecondary,
+    required this.action,
+    required this.backgroundPrimary,
+    required this.backgroundSecondary,
   });
 }
 
 final colorsDark = AppColors(
   textPrimary: DarkColors.textPrimary,
   textSecondary: DarkColors.textSecondary,
+  action: DarkColors.action,
+  backgroundPrimary: DarkColors.backgroundPrimary,
+  backgroundSecondary: DarkColors.backgroundSecondary,
 );
 
 extension AppColorsExtension on BuildContext {
